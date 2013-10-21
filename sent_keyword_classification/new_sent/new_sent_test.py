260d0062b1fa42bb8a21cfefcab668a62d8f8fa5
tkrugg <tkrugg@live.fr>
2013-10-21 19:40:44 +0200

    arred test file

--- sent_keyword_classification/new_sent/new_sent_test.py
+++ sent_keyword_classification/new_sent/new_sent_test.py
@@ -35,73 +35,67 @@ class Post:
     def __repr__(self):
         return self.content + "/" + str(self.isPositive)
 
+# analyze posts 
+p = open("pos.txt", "r")
+posPosts = p.readlines()
+n = open("neg.txt", "r")
+negPosts = n.readlines()
+
+
+NEG_POSTS =  map(lambda post : Post(post, False), negPosts)
+POS_POSTS = map(lambda post : Post(post, True), posPosts)
+TAGGED_POSTS = NEG_POSTS + POS_POSTS
+
+#### TEST POSTS
+ratio = .8
+limit_index = int(.8 * len(NEG_POSTS))
+TEST_NEG_POSTS = NEG_POSTS[limit_index:]
+NEG_POSTS = NEG_POSTS[:limit_index]
+
+limit_index = int(.8 * len(POS_POSTS))
+TEST_POS_POSTS = POS_POSTS[limit_index:]
+POS_POSTS = POS_POSTS[:limit_index]
+####
 if os.path.exists("save.p"):
     classifier = pickle.load( open( "save.p", "rb" ) )
 
 else:
-    # analyze posts 
-    p = open("pos.txt", "r")
-    posPosts = p.readlines()
-    n = open("neg.txt", "r")
-    negPosts = n.readlines()
-
-
-    NEG_POSTS =  map(lambda post : Post(post, False), negPosts)
-    POS_POSTS = map(lambda post : Post(post, True), posPosts)
-    TAGGED_POSTS = NEG_POSTS + POS_POSTS
+    print 'reginerating...'
 
     negfeats = [(word_feats(post.getWords()), 'neg') for post in NEG_POSTS]
     posfeats = [(word_feats(post.getWords()), 'pos') for post in POS_POSTS]
 
     trainfeats = negfeats + posfeats
     classifier = NaiveBayesClassifier.train(trainfeats)
-    print 'reginerating...'
     pickle.dump(classifier, open("save.p", "wb"))
 #for testfeat in testfeats:
     #print('{s} => {c}'.format(s = testfeat, c = classifier.classify(testfeat)))
 
-
-t = open("testneg.csv", "r")
-testPosts = t.readlines()
-TEST_POSTS = map(lambda post : Post(post, True), testPosts)
-testfeats = [word_feats(post.getWords()) for post in TEST_POSTS]
+print "testing positives"
+testfeats = [word_feats(post.getWords()) for post in TEST_POS_POSTS]
 wrongs = 0
-for test in testPosts:
-    post = Post(test, True)
+for test in TEST_POS_POSTS:
+    post = test
     feat = word_feats(post.getWords())
     classif = classifier.classify(feat)
-    if classif != "neg":
+    if classif == "neg":
         wrongs = wrongs + 1
-    print('{s} => {c}'.format(s = post.content, c = classif))
-print "wrongs: ", wrongs, " over ", len(testPosts)
-print "error rate: ", float(wrongs)/len(testPosts)
+    #print('{s} => {c}'.format(s = post.content, c = classif))
+print "wrongs: ", wrongs, " over ", len(TEST_POS_POSTS)
+print "error rate: ", float(wrongs)/len(TEST_POS_POSTS)
 
 
-
-
-#print TAGGED_POSTS
-#print WORDS
-#print ALL_WORDS
-
-## order by frequency
-#wordfreq = nltk.FreqDist(ALL_WORDS)
-#ORDERED_WORDS = wordfreq.keys()
-#print ORDERED_WORDS 
-
-#FILTERED_ORDERED_WORDS = filter(lambda w :  not w in stopwords.words("english"), ORDERED_WORDS)
-
-#print FILTERED_ORDERED_WORDS
-
-#def feature_extractor(doc):
-#    docwords = set(doc)
-#    features = {}
-#    for i in FILTERED_ORDERED_WORDS:
-#        features['contains(%s)' % i] = (i in docwords)
-#    return features 
-
-#training_set = nltk.classify.apply_features(feature_extractor, TAGGED_POSTS)
-#classifier = nltk.NaiveBayesClassifier.train(training_set)
-
-#print classifier.show_most_informative_features(n=30)
+print "testing negatives"
+testfeats = [word_feats(post.getWords()) for post in TEST_NEG_POSTS]
+wrongs = 0
+for test in TEST_NEG_POSTS:
+    post = test
+    feat = word_feats(post.getWords())
+    classif = classifier.classify(feat)
+    if classif == "pos":
+        wrongs = wrongs + 1
+    #print('{s} => {c}'.format(s = post.content, c = classif))
+print "wrongs: ", wrongs, " over ", len(TEST_NEG_POSTS)
+print "error rate: ", float(wrongs)/len(TEST_NEG_POSTS)
 
 
